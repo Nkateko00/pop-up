465ba00d7eeaccf0b50c6ac0018ccc61a7026911
kagiso101 <hadebekagiso3@gmail.com>
2020-09-30 10:36:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//Require all modules needed
+
+let express = require('express');//to create web apps
+var exphbs = require('express-handlebars');//to render templates
+const bodyParser = require('body-parser');//require body parser for htm functionality
+const flash = require('express-flash');
+const session = require('express-session');
+
+let app = express()
+
+//setup handlebars ,Body-parser and public
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ layoutsDir: './views/layouts' }));
+
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+    secret: 'my express flash string',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
+//to use css
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+//routes
+app.get('/', function (req, res) {
+
+    res.render('home'); 
+})
+
+app.post('/cities')
+
+//Port setup
+const PORT = process.env.PORT || 3008;
+
+ app.listen(PORT, function () {
+    console.log('App starting on port :' + PORT);
+});
---
+++ popup.js
@@ -0,0 +1,31 @@
+module.exports = function () {
+
+
+    const pg = require("pg");
+    const Pool = pg.Pool;
+    const connectionString = process.env.DATABASE_URL || 'postgresql://kagiso:123@localhost:5432/popup';
+    const pool = new Pool({
+        connectionString
+    });
+
+
+//adds city selected to database 
+    async function addCity(city) {
+    
+        const checking = await pool.query(`select id from popup where reqion = $1`, [city])
+        if (checking.rowCount === 0) {
+            await pool.query(`insert into popup (region, counter) values ($1, 0)`, [city]);
+        }
+        await pool.query(`update popup set counter = counter+1 where item = $1`, [city])
+    }
+
+
+    return {
+
+      addCity
+
+    }
+}
+
+// `select * distict type from popup where reqion = 'cpt'`
+
